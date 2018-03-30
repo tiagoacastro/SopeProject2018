c3e8501eb678054bca3f1f0b23215a57ebd4a3f5
Beatriz <bea.smendes98@gmail.com>
2018-03-30 16:40:45 +0100

    Added main.

---
+++ simpgrep.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include<string.h>
+
+int main(int argc,char *argv[])
+{
+return 0;
+}
