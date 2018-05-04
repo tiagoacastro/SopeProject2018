c0d3276697675f81b9b7a997c56c1369c3ce5125
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-04 17:24:43 +0100

    changes

--- Project/client.c
+++ Project/client.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
 
-int main(int argc,char *argv[], char* env[]){
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
+
+  if (argc == 4)
+    printf("ARGS: %s | %s | %s\n", argv[1], argv[2], argv[3]);
+
+  sleep(1);
+
   return 0;
 }
