509d598fbaaa52db4f65c0c05f84a28ac69440b5
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-07 20:18:33 +0100

    client.c devera estar a dar, falta cenas do clog, server da segfault

--- Project/client.c
+++ Project/client.c
@@ -7,32 +7,41 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
+  int pid = getpid();
+  printf("** Running process %d (PGID %d) **\n", pid, getpgrp());
 
   if (argc == 4)
     printf("ARGS: %s | %s | %s\n", argv[1], argv[2], argv[3]);
 
+  //send request
+  int requests = open("requests", O_WRONLY);
+  if(requests == -1){
+    printf("Error opening requests FIFO\n");
+    return -3;
+  }
+
+  char message[100];
+  int messagelen;
+  sprintf(message,"%d %d %d", pid, argv[2], argv[3]);
+  messagelen=strlen(message)+1;
+  write(requests,message,messagelen);
+
+  close(requests);
+
+  //get status
   char sn[12];
-  int pid = getpid();
+
   sprintf(sn, "ans%d", pid);
   printf("FIFO name: %s\n", sn);
-
   if(mkfifo(sn, 0660) == -1){
     printf("Error creating status FIFO\n");
     return -1;
   }
 
-  printf("r\n");
-
   int status = open(sn, O_RDONLY);
   if(status == -1){
     printf("Error opening status FIFO\n");
     return -2;
-  }
-  int requests = open(sn, O_WRONLY);
-  if(requests == -1){
-    printf("Error opening requests FIFO\n");
-    return -3;
   }
 
   char* ret;
@@ -42,14 +51,12 @@ int main(int argc, char *argv[]) {
   time_t seconds = argv[1];
   endwait = start + seconds;
 
-  while (start < endwait)
+  while (start < endwait && ret == NULL)
   {
       ret = fgets(temp, 100, status);
-      sleep(1);
       start = time(NULL);
   }
 
-  close(requests);
   close(status);
 
   return 0;
